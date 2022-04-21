16cb2d89eee6ee825bb19f2eb033c7bc618244b3
Tymoteusz Karczewski <tymkar2020@gmail.com>
2022-04-21 17:16:54 +0200

    remove commented code

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -1,19 +1,3 @@
-// import axios from "axios";
-
-// export const baseUrl = "https://bayut.p.rapidapi.com";
-
-// export const fetchApi = async (url) => {
-//   const { data } = await axios.get(
-//     (url,
-//     {
-//       headers: {
-//         "X-RapidAPI-Host": "bayut.p.rapidapi.com",
-//         "X-RapidAPI-Key": "183b5bdd1dmsh2c4c89955de0a3ep177854jsnd27bb45695a1",
-//       },
-//     })
-//   );
-// };
-
 import axios from "axios";
 
 export const baseUrl = "https://bayut.p.rapidapi.com";
